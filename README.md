24adad19f63edd1f4e1da63b6973c56fea29bbc5
Cristhian Solís <cristhian.solis.m@gmail.com>
2025-08-15 01:34:52 -0400

    Add FastAPI client simulation notebook for model prediction

--- README.md
+++ README.md
@@ -1,27 +1,110 @@
-# Deploy FastAPI on Render
+# FastAPI-Breast-Cancer-Predictor
 
-Use this repo as a template to deploy a Python [FastAPI](https://fastapi.tiangolo.com) service on Render.
+Este proyecto implementa una **API en FastAPI** que carga un modelo previamente entrenado y expone un endpoint `/predict` para realizar predicciones a partir de datos enviados en formato JSON.
 
-See https://render.com/docs/deploy-fastapi or follow the steps below:
+## Instalación y configuración local
 
-## Manual Steps
+1. **Clonar este repositorio**:
 
-1. You may use this repository directly or [create your own repository from this template](https://github.com/render-examples/fastapi/generate) if you'd like to customize the code.
-2. Create a new Web Service on Render.
-3. Specify the URL to your new repository or this repository.
-4. Render will automatically detect that you are deploying a Python service and use `pip` to download the dependencies.
-5. Specify the following as the Start Command.
+```bash
+git clone https://github.com/csolis97/FastAPI-Breast-Cancer-Predictor
+cd FastAPI-Breast-Cancer-Predictor
+```
+2. **Abre CMD y entra a la carpeta donde clonaste el  proyecto (ajusta tu ruta)**:
 
-    ```shell
-    uvicorn main:app --host 0.0.0.0 --port $PORT
-    ```
+```bash
+cd "C:\Users\xx\Desktop\FastAPI-Breast-Cancer-Predictor"
+```
+3. **Crear un entorno virtual** (opcional pero recomendado):
 
-6. Click Create Web Service.
+```bash
+python -m venv venv
+```
+4. **Activar el entorno virtual** - **Windows (CMD)**:
 
-Or simply click:
+```bash
+venv\Scripts\activate.bat
+```
+5. **Instalar las dependencias**:
+```bash
+pip install -r requirements.txt
+```
+5. **Iniciar el servidor local**:
+```bash
+uvicorn main:app --reload
+```
 
-[![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)](https://render.com/deploy?repo=https://github.com/render-examples/fastapi)
+6. **Abrir en el navegador**: [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
 
-## Thanks
+---
 
-Thanks to [Harish](https://harishgarg.com) for the [inspiration to create a FastAPI quickstart for Render](https://twitter.com/harishkgarg/status/1435084018677010434) and for some sample code!
+## Uso del endpoint `/predict`
+
+### **Método:**
+
+`POST`
+
+### **URL local:**
+
+`http://127.0.0.1:8000/predict`
+
+### **JSON de entrada esperado**:
+
+```json
+{
+    "mean_radius": 14.127292,
+    "mean_texture": 19.289649,
+    "mean_perimeter": 91.969033,
+    "mean_area": 654.889104,
+    "mean_smoothness": 0.096360,
+    "mean_compactness": 0.104341,
+    "mean_concavity": 0.088799,
+    "mean_concave_points": 0.048919,
+    "mean_symmetry": 0.181162,
+    "mean_fractal_dimension": 0.062798,
+    "radius_error": 0.405172,
+    "texture_error": 1.216853,
+    "perimeter_error": 2.866059,
+    "area_error": 40.337079,
+    "smoothness_error": 0.007041,
+    "compactness_error": 0.025478,
+    "concavity_error": 0.031894,
+    "concave_points_error": 0.011796,
+    "symmetry_error": 0.020542,
+    "fractal_dimension_error": 0.003795,
+    "worst_radius": 16.269190,
+    "worst_texture": 25.677223,
+    "worst_perimeter": 107.261213,
+    "worst_area": 880.583128,
+    "worst_smoothness": 0.132369,
+    "worst_compactness": 0.254265,
+    "worst_concavity": 0.32,
+    "worst_concave_points": 0.114606,
+    "worst_symmetry": 0.290076,
+    "worst_fractal_dimension": 0.083946
+}
+```
+
+### **Ejemplo de respuesta**:
+
+```json
+{
+    "'prediction": "1",
+    "probabilidad": 0.5332673580926112
+}
+```
+
+---
+## Estructura del repositorio
+
+```
+Tarea2/
+│── main.py          # Archivo principal de la API
+│── modelo.pkl       # Modelo entrenado
+│── requirements.txt # Dependencias
+│── README.md        # Documentación completa
+```
+
+---
+## Autor
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-Cristhian%20Solís-blue?logo=linkedin&style=for-the-badge)](https://www.linkedin.com/in/csolism97/)
--- main.py
+++ main.py
@@ -1,14 +1,123 @@
-from typing import Optional
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field, validator
+import joblib
+import numpy as np
+import os
 
-from fastapi import FastAPI
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "models", "model.pkl")
+scaler_path = os.path.join(BASE_DIR, "models", "scaler.pkl")
+print("BASE_DIR =", BASE_DIR)
+print("model_path =", model_path)
+print("scaler_path =", scaler_path)
+print("¿Existe model_path?", os.path.exists(model_path))
+print("¿Existe scaler_path?", os.path.exists(scaler_path))
+
+try:
+    model = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+except FileNotFoundError:
+    print("Error: scaler.pkl o modelo.pkl no encontrados.")
+    model = None
+    scaler = None
 
 app = FastAPI()
+# Definición del modelo de entrada con campos example media real
+class InputData(BaseModel):
+    mean_radius: float = Field(..., example=14.127292)
+    mean_texture: float = Field(..., example=19.289649)
+    mean_perimeter: float = Field(..., example=91.969033)
+    mean_area: float = Field(..., example=654.889104)
+    mean_smoothness: float = Field(..., example=0.096360)
+    mean_compactness: float = Field(..., example=0.104341)
+    mean_concavity: float = Field(..., example=0.088799)
+    mean_concave_points: float = Field(..., example=0.048919)
+    mean_symmetry: float = Field(..., example=0.181162)
+    mean_fractal_dimension: float = Field(..., example=0.062798)
+    radius_error: float = Field(..., example=0.405172)
+    texture_error: float = Field(..., example=1.216853)
+    perimeter_error: float = Field(..., example=2.866059)
+    area_error: float = Field(..., example=40.337079)
+    smoothness_error: float = Field(..., example=0.007041)
+    compactness_error: float = Field(..., example=0.025478)
+    concavity_error: float = Field(..., example=0.031894)
+    concave_points_error: float = Field(..., example=0.011796)
+    symmetry_error: float = Field(..., example=0.020542)
+    fractal_dimension_error: float = Field(..., example=0.003795)
+    worst_radius: float = Field(..., example=16.269190)
+    worst_texture: float = Field(..., example=25.677223)
+    worst_perimeter: float = Field(..., example=107.261213)
+    worst_area: float = Field(..., example=880.583128)
+    worst_smoothness: float = Field(..., example=0.132369)
+    worst_compactness: float = Field(..., example=0.254265)
+    worst_concavity: float = Field(..., example=0.272188)
+    worst_concave_points: float = Field(..., example=0.114606)
+    worst_symmetry: float = Field(..., example=0.290076)
+    worst_fractal_dimension: float = Field(..., example=0.083946)
 
-
+    @validator('*')
+    def values(cls, v):
+        if v < 0:
+            raise ValueError('Los valores no pueden ser negativos')
+        if v > 9999:
+            raise ValueError('Valor atípico detectado: 9999 no es válido')
+        return v
+    
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "API funcionando. Usar POST /predict para hacer predicciones."}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+def predict(data: InputData):
+    if scaler is None or model is None:
+        raise HTTPException(status_code=500, detail="Modelo o scaler no están cargados correctamente.")
+    try:
+        input_data = np.array([[
+            data.mean_radius,
+            data.mean_texture,
+            data.mean_perimeter,
+            data.mean_area,
+            data.mean_smoothness,
+            data.mean_compactness,
+            data.mean_concavity,
+            data.mean_concave_points,
+            data.mean_symmetry,
+            data.mean_fractal_dimension,
+            data.radius_error,
+            data.texture_error,
+            data.perimeter_error,
+            data.area_error,
+            data.smoothness_error,
+            data.compactness_error,
+            data.concavity_error,
+            data.concave_points_error,
+            data.symmetry_error,
+            data.fractal_dimension_error,
+            data.worst_radius,
+            data.worst_texture,
+            data.worst_perimeter,
+            data.worst_area,
+            data.worst_smoothness,
+            data.worst_compactness,
+            data.worst_concavity,
+            data.worst_concave_points,
+            data.worst_symmetry,
+            data.worst_fractal_dimension
+        ]])
+
+        # Escalado
+        features_scaled = scaler.transform(input_data)
+
+        # Predicción
+        prediction = model.predict(features_scaled)
+        pred_proba = model.predict_proba(features_scaled)[:,1] # Probabilidad de clase positiva
+
+        return {
+            "prediction": int(prediction[0]),
+            "probability": float(pred_proba[0])
+        }
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Error en los datos: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno: {e}")
